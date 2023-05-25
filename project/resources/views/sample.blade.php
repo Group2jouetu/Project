3684f2e0a1e33698c5bcf5ff945f72955cbdc4bd
mysonjony <100464556+mysonjony@users.noreply.github.com>
2023-05-25 21:15:25 +0900

    Update sample.blade.php

--- project/resources/views/sample.blade.php
+++ project/resources/views/sample.blade.php
@@ -6,8 +6,4 @@
             <p>{{ $item }}</p>
         @endforeach
     </div>
-
-
-    <script async defer
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBJoO2BmaGEIp_ud8Mctyd5gLDWrEYzMFA&callback=initMap"></script>
 @endsection
