2aaf181a2cc0a9099b2f86d0148d5cd2d296cefe
opipinopi <toyotaka.me@gmail.com>
2023-06-01 14:20:34 +0900

    test

--- project/resources/views/layout/layout.blade.php
+++ project/resources/views/layout/layout.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/css/map.css">
     <title>@yield('title')</title>
+    @vite('resources/sass/app.scss')
 </head>
 
 <body>
