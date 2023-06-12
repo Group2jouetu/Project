3049f7096e6e2d10bd09ecac39d5b5d91027b703
DESKTOP-R1MDEG5\shizuo <zusi0@icloud.com>
2023-06-12 19:38:52 +0900

    モーダルウィンドウの表示を追加

--- resources/views/snsmapping.blade.php
+++ resources/views/snsmapping.blade.php
@@ -2,38 +2,45 @@
     <div id="map"></div>
 
     {{-- <button onclick="openMyMap()">モデルコース機能を開く</button> --}}
-    <form action="/snsInput" method="post" enctype="multipart/form-data">
-        {{ csrf_field() }}
-        <input type="file" name="image" id="image" class="image" />
-        <p>本文</p>
-        <input type="text" name="pin_title" />
-        <input type="hidden" name="lat" id="get-lat" />
-        <input type="hidden" name="lng" id="get-lng" />
-        {{-- <button>キャンセル
-            <script>
-                window.history.back();
-            </script>
-        </button> --}}
-        {{-- <input type="submit" value="キャンセル"> --}}
-        <input type="submit" value="登録">
-    </form>
+    
+    <!-- ここからモーダルウィンドウ -->
+    <div id="modal" class="modal">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <h3>新規登録</h3>
+            <form action="/snsInput" method="post" enctype="multipart/form-data">
+                {{ csrf_field() }}
+                <input type="file" name="image" id="image-input" class="image" />
+                {{-- 選択された画像を表示 --}}
+                <img id="preview-image" src="#" style="max-width: 256px; height: auto;">
+                <p>場所の名前を入力してください</p>
+                <input type="text"   name="pin_title" id="title" placeholder="テキストを入力してください。" value="" />
+                <input type="hidden" name="lat" id="get-lat" />
+                <input type="hidden" name="lng" id="get-lng" />
+                {{-- <button>キャンセル
+                    <script>
+                        window.history.back();
+                    </script>
+                </button> --}}
+                {{-- <input type="submit" value="キャンセル"> --}}
+                <input type="submit" value="登録">
+            </form>
+        </div>
+    </div>
+    <!-- ここまで -->
 
-    <div id="image-container" style="min-width: 256px; height: auto;"></div>
 
     {{-- 選択した画像を表示する --}}
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
-        $(document).ready(function() {
-            $('#image-input').on('change', function(e) {
-                var file = e.target.files[0];
-                var reader = new FileReader();
+        document.getElementById('image-input').addEventListener('change', function(e) {
+            var file = e.target.files[0];
+            var reader = new FileReader();
 
-                reader.onload = function(e) {
-                    var imageUrl = e.target.result;
-                    $('#image-container').html('<img src="' + imageUrl + '">');
-                };
-                reader.readAsDataURL(file);
-            });
+            reader.onload = function(e) {
+                document.getElementById('preview-image').setAttribute('src', e.target.result);
+            }
+
+            reader.readAsDataURL(file);
         });
 
         // googleマップ
@@ -52,22 +59,50 @@
                 // すでに立てたマーカーがあった場合、そのマーカーを削除
                 if (click_marker) {
                     click_marker.setMap(null);
+                    // 入力してあった情報をリセット
+                    document.getElementById('preview-image').setAttribute('src', "");
+                    document.getElementById('image-input').value = "";
+                    document.getElementById('title').value = "";
                 }
 
                 //マーカーを立てた場所を画面中央にする
                 this.panTo(e.latLng);
-                //マーカーを立てる
+
+                // クリックした位置の緯度経度を保存しておく
                 document.getElementById('get-lat').value = e.latLng.lat();
                 document.getElementById('get-lng').value = e.latLng.lng();
+
+                //マーカーを立てる
                 e.latLng.lat();
-                // console.log(e.latLng[Scopes][0]);
                 click_marker = new google.maps.Marker({
                     position: e.latLng,
                     map: map,
                     title: e.latLng.toString(),
                     animation: google.maps.Animation.DROP
                 });
-                // console.log(click_marker.position);
+
+                // ここからマーカー立てたらモーダルウィンドウ開く処理
+                var modal = document.getElementById("modal");
+                var button = document.getElementById("modalButton");
+                var close = document.getElementsByClassName("close")[0];
+
+                function openModalWindow(marker) {
+                    modal.style.display = "block";
+                }
+
+                close.onclick = function() {
+                    modal.style.display = "none";
+                }
+
+                window.onclick = function(event) {
+                    if (event.target == modal) {
+                        modal.style.display = "none";
+                    }
+                }
+                //  ピンを差したらモーダルウィンドウ表示する
+                openModalWindow(click_marker)
+
+                // ここまで
             });
         }
     </script>
