9cf83e3265391e7e5fa667f58ff5a21009dcde8b
opipinopi <toyotaka.me@gmail.com>
2023-06-12 17:56:43 +0900

    add:冠画像、ジャンルごとのランキング機能

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -9,8 +9,32 @@ class RankingController extends Controller
     public function index () 
     {
         $ranking = DB::select('select * from pins order by like_count desc');
-        $data = ['ranking' => $ranking];
-        return view('ranking', $data);
+
+        // 食べ物
+        $food = DB::select('select * from pins where genre=1 order by like_count desc');
+
+        // 宿泊
+        $hotel = DB::select('select * from pins where genre=2 order by like_count desc');
+
+         // 文化
+        $culture = DB::select('select * from pins where genre=3 order by like_count desc');
+
+         
+        // 遊び
+        $amusement = DB::select('select * from pins where genre=4 order by like_count desc');
+
         
+        // 自然
+        $nature = DB::select('select * from pins where genre=5 order by like_count desc');
+
+
+        return view('ranking',[
+            'ranking' => $ranking,
+            'food' => $food,
+            'hotel' => $hotel,
+            'culture' => $culture,
+            'amusement' => $amusement,
+            'nature' => $nature,
+        ]);
     }
 }
