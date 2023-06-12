dfa7ca70345bf358fc0bdf649b152c0a9e5ab466
DESKTOP-R1MDEG5\shizuo <zusi0@icloud.com>
2023-06-12 17:04:18 +0900

    lat,lngの情報取得、Model作成など

--- app/Http/Controllers/SnsMappingController.php
+++ app/Http/Controllers/SnsMappingController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pin;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SnsMappingController extends Controller
 {
-    //
+    
     public function store(Request $request){
-        $request->file('image')->store('public/images');
+
+        $pin = new Pin();
+        
+        if($request->file('image')){
+            $pin->picture = $request->file('image')->store('public/images');
+        }
+        
+        $pin->latitude = $request->lat;
+        $pin->longitude = $request->lng;
+        $pin->save();
     }
 }
--- resources/views/snsmapping.blade.php
+++ resources/views/snsmapping.blade.php
@@ -1,83 +1,78 @@
 @extends('layout.layout') @section('title', 'SNSマップページ') @section('content')
-{{-- <div id="map"></div> --}}
+    <div id="map"></div>
 
-{{-- <button onclick="openMyMap()">モデルコース機能を開く</button> --}}
+    {{-- <button onclick="openMyMap()">モデルコース機能を開く</button> --}}
+    <form action="/snsInput" method="post" enctype="multipart/form-data">
+        {{ csrf_field() }}
+        <input type="file" name="image" id="image" class="image" />
+        <p>本文</p>
+        <input type="text" name="pin_title" />
+        <input type="hidden" name="lat" id="get-lat" />
+        <input type="hidden" name="lng" id="get-lng" />
+        {{-- <button>キャンセル
+            <script>
+                window.history.back();
+            </script>
+        </button> --}}
+        {{-- <input type="submit" value="キャンセル"> --}}
+        <input type="submit" value="登録">
+    </form>
 
-<form action="/snsInput" method="post" enctype="multipart/form-data">
-    {{ csrf_field() }}
-    <input type="file" name="image" id="image" class="image" />
-    <p>本文</p>
-    {{-- <input type="text" name="text" /> --}}
-    {{-- <button>キャンセル
-        <script>
-            window.history.back();
-        </script>
-    </button> --}}
-    {{-- <input type="submit" value="キャンセル"> --}}
-    <input type="submit" value="登録">
-</form>
-@if ($message = Session::get('success'))
-        <p>{{ $message }}</p>
-        <img src="{{ asset('images/' . Session::get('image')) }}" width="300">
-    @endif
-<div id="image-container" style="min-width: 256px; height: auto;"></div>
+    <div id="image-container" style="min-width: 256px; height: auto;"></div>
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-    $(document).ready(function() {
-        $('#image-input').on('change', function(e) {
-            var file = e.target.files[0];
-            var reader = new FileReader();
+    {{-- 選択した画像を表示する --}}
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#image-input').on('change', function(e) {
+                var file = e.target.files[0];
+                var reader = new FileReader();
 
-            reader.onload = function(e) {
-                var imageUrl = e.target.result;
-                $('#image-container').html('<img src="' + imageUrl + '">');
-            };
-
-            reader.readAsDataURL(file);
-        });
-    });
-
-    function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-            center: {
-                lat: 37.1478,
-                lng: 138.236
-            },
-            zoom: 12
-        });
-
-        var click_marker;
-        //mapクリック時の処理
-        map.addListener("click", function(e) {
-            // すでに立てたマーカーがあった場合、そのマーカーを削除
-            if (click_marker) {
-                click_marker.setMap(null);
-            }
-
-            //マーカーを立てた場所を画面中央にする
-            this.panTo(e.latLng);
-            //マーカーを立てる
-            click_marker = new google.maps.Marker({
-                position: e.latLng,
-                map: map,
-                title: e.latLng.toString(),
-                animation: google.maps.Animation.DROP
+                reader.onload = function(e) {
+                    var imageUrl = e.target.result;
+                    $('#image-container').html('<img src="' + imageUrl + '">');
+                };
+                reader.readAsDataURL(file);
             });
         });
 
-    }
+        // googleマップ
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById('map'), {
+                center: {
+                    lat: 37.1478,
+                    lng: 138.236
+                },
+                zoom: 12
+            });
 
-    function openMyMap() {
-        window.open("https://www.google.com/maps/d/edit?mid=1OF1wBE2l6vNNrmU7F-b2OBOi5Sc3Awg&usp=sharing");
-    }
-</script>
+            //mapクリック時の処理
+            var click_marker;
+            map.addListener("click", function(e) {
+                // すでに立てたマーカーがあった場合、そのマーカーを削除
+                if (click_marker) {
+                    click_marker.setMap(null);
+                }
 
-<script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBJoO2BmaGEIp_ud8Mctyd5gLDWrEYzMFA&callback=initMap"></script>
+                //マーカーを立てた場所を画面中央にする
+                this.panTo(e.latLng);
+                //マーカーを立てる
+                document.getElementById('get-lat').value = e.latLng.lat();
+                document.getElementById('get-lng').value = e.latLng.lng();
+                e.latLng.lat();
+                // console.log(e.latLng[Scopes][0]);
+                click_marker = new google.maps.Marker({
+                    position: e.latLng,
+                    map: map,
+                    title: e.latLng.toString(),
+                    animation: google.maps.Animation.DROP
+                });
+                // console.log(click_marker.position);
+            });
+        }
+    </script>
 
-{{--
-<link rel="stylesheet" href="{{ asset('css/map.css') }}">
-<!-- map.cssのパスが正しい場所になるように修正 --> --}}
+    <script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBJoO2BmaGEIp_ud8Mctyd5gLDWrEYzMFA&callback=initMap"></script>
 
 @endsection
