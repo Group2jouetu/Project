83f152e86a8e31ff96b210d756d534f19c035ab6
opipinopi <toyotaka.me@gmail.com>
2023-06-12 18:01:13 +0900

    ランキング機能のcssリンク追加

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/css/map.css">
+    <link rel="stylesheet" href="/css/ranking.css">
     <title>@yield('title')</title>
     @vite('resources/sass/app.scss')
 </head>
