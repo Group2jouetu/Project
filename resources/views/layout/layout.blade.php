3c567462ee35e51ef55670fc1f98baf794cfe131
opipinopi <toyotaka.me@gmail.com>
2023-06-13 12:39:00 +0900

    add:pinsテーブルへの仮データ change:ランキング機能更新

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/css/map.css">
     <link rel="stylesheet" href="/css/ranking.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <title>@yield('title')</title>
     @vite('resources/sass/app.scss')
 </head>
