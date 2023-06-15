79602f4f7d4c64c4752eca1d079940a7367de749
opipinopi <toyotaka.me@gmail.com>
2023-06-15 15:33:50 +0900

    add:ボトムナビゲーション追加

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/css/map.css">
     <link rel="stylesheet" href="/css/ranking.css">
+    <link rel="stylesheet" href="/css/bottomNav.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <title>@yield('title')</title>
     {{-- bootstrap --}}
@@ -15,9 +16,11 @@
 </head>
 
 <body>
-    <div class="main_content">
-        @yield('content')
-    </div>
+        @extends('bottomNav')
+
+        <div class="main_content">
+            @yield('content')
+        </div>
 </body>
 
 </html>
