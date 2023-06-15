33324f6e76f68a34988b3ccda31d799d5aec36ad
opipinopi <toyotaka.me@gmail.com>
2023-06-15 15:42:31 +0900

    change:ボトムナビゲーションのリンク、デザイン変更

--- resources/views/bottomNav.blade.php
+++ resources/views/bottomNav.blade.php
@@ -3,7 +3,7 @@
     <ul class="menu">
 
         <li>
-            <a href=""><i class="fa-solid fa-map fa-2x"></i><br>
+            <a href="/map"><i class="fa-solid fa-map fa-2x"></i><br>
             <span class="iconName map">マップ</span></a>
         </li>
 
@@ -17,6 +17,11 @@
         <li>
             <a href=""><i class="fa-solid fa-road fa-2x"></i><br>
             <span class="iconName model">モデルコース</span></a>
+        </li>
+
+        <li>
+            <a href="/ranking"><i class="fa-solid fa-crown fa-2x"></i><br>
+            <span class="iconName model">ランキング</span></a>
         </li>
 
         <li>
