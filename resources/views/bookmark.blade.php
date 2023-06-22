05321f053111c596c179f01088a26de64b39b6ac
mumuyume <kittoarumuutairiku@gmail.com>
2023-06-22 16:04:18 +0900

    旅のしおりページ デザイン修正

--- resources/views/bookmark.blade.php
+++ resources/views/bookmark.blade.php
@@ -18,11 +18,26 @@
         @foreach($bookmarks as $bookmark)
         <div class="col">
           <div class="card shadow-sm">
-            <input type="image" src="{{ asset('storage/images').'/'.$bookmark->picture }}" class="btn bd-placeholder-img card-img-top img-fluid" width="100%" height="225" alt="image" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <input type="image" src="{{ asset('storage/images').'/'.$bookmark->picture }}" class="btn bd-placeholder-img card-img-top img-fluid" width="100%" height="225" alt="image" data-bs-toggle="modal" data-bs-target="#exampleModal{{$bookmark->pin_id}}">
 
             <div class="card-body">
               <p class="card-title">{{$bookmark->pin_name}}</p>
-              <p class="card-text">{{$bookmark->detail}}ここに説明文が入りますここに説明文が入りますここに説明文が入りますここに説明文が入ります</p>
+            </div>
+          </div>
+        </div>
+        <!-- モーダル -->
+        <div class="modal fade" id="exampleModal{{$bookmark->pin_id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <!-- pictureとdetailを表示 -->
+                <img src="{{ asset('storage/images').'/'.$bookmark->picture }}" class="btn bd-placeholder-img card-img-top img-fluid" width="100%" height="225" alt="image">
+                <p>{{$bookmark->detail}}ここに説明文が入りますここに説明文が入りますここに説明文が入りますここに説明文が入ります</p>
+              </div>
             </div>
           </div>
         </div>
@@ -31,18 +46,5 @@
     </div>
   </div>
 
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <!-- pictureとdetailを表示 -->
-      </div>
-    </div>
-  </div>
-</div>
 
 @endsection
