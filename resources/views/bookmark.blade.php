2b1cbf4ce0860ddd36a630dd800a884c55074c34
mumuyume <kittoarumuutairiku@gmail.com>
2023-06-22 15:45:35 +0900

    旅のしおりページ viewファイル編集

--- resources/views/bookmark.blade.php
+++ resources/views/bookmark.blade.php
@@ -10,17 +10,25 @@
 
 @section('content')
 
-<div class="container-fluid">
-    <div class="row">
+<div class="album py-5 bg-light">
+    <div class="container">
+
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
         <!-- 旅のしおりのでも処理 -->
         @foreach($bookmarks as $bookmark)
-        <div class="col-4 text-center">
-            <!-- storageフォルダに画像を入れる処理ができれば画像が表示される -->
-            <img src="{{ asset('storage/images').'/'.$bookmark->picture }}" class="img-fluid" alt="" style="max-height: 100px;"><br>
-            <p>{{$bookmark->pin_name}}</p>
+        <div class="col">
+          <div class="card shadow-sm">
+            <img src="{{ asset('storage/images').'/'.$bookmark->picture }}" class="bd-placeholder-img card-img-top img-fluid" width="100%" height="225" alt="image">
+
+            <div class="card-body">
+              <p class="card-title">{{$bookmark->pin_name}}</p>
+              <p class="card-text">{{$bookmark->detail}}ここに説明文が入りますここに説明文が入りますここに説明文が入りますここに説明文が入ります</p>
+            </div>
+          </div>
         </div>
         @endforeach
+      </div>
     </div>
-</div>
+  </div>
 
 @endsection
