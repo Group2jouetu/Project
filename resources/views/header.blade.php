074f23ba777a8aa0ed391903b286f981349ae588
Taipon0125 <Sandar0125@gmail.com>
2023-06-22 15:31:55 +0900

    ロゴをクリックしたらトップページ(map)に飛ぶように変更しました

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -10,7 +10,7 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">
+                <a class="navbar-brand" href="map">
                     <img class="" src="img/jouetsuGo_icon.png" alt="上越GOアイコン">
                 </a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
