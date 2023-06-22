04a1d22f7dc34602f8993b69269be8054197c4a1
kaizuchan <kaizu715@icloud.com>
2023-06-22 15:56:58 +0900

    レイアウト改変２

--- resources/views/bookmark.blade.php
+++ resources/views/bookmark.blade.php
@@ -18,7 +18,7 @@
         @foreach($bookmarks as $bookmark)
         <div class="col">
           <div class="card shadow-sm">
-            <img src="{{ asset('storage/images').'/'.$bookmark->picture }}" class="bd-placeholder-img card-img-top img-fluid" width="100%" height="225" alt="image">
+            <input type="image" src="{{ asset('storage/images').'/'.$bookmark->picture }}" class="btn bd-placeholder-img card-img-top img-fluid" width="100%" height="225" alt="image" data-bs-toggle="modal" data-bs-target="#exampleModal">
 
             <div class="card-body">
               <p class="card-title">{{$bookmark->pin_name}}</p>
@@ -30,5 +30,19 @@
       </div>
     </div>
   </div>
+
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <!-- pictureとdetailを表示 -->
+      </div>
+    </div>
+  </div>
+</div>
 
 @endsection
