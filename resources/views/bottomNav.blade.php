21366b6f6b23211512e5e39ba76ed4e7f5c09702
opipinopi <toyotaka.me@gmail.com>
2023-06-22 19:22:04 +0900

    change:ボトムナビゲーションデザイン修正

--- resources/views/bottomNav.blade.php
+++ resources/views/bottomNav.blade.php
@@ -3,29 +3,29 @@
     <ul class="menu">
 
         <li class="map">
-            <a href="/map"><i class="fa-solid fa-map fa-2x"></i><br>
+            <a href="/map"><i class="fa-regular fa-map fa-2x"></i><br>
             <span class="iconName">マップ</span></a>
         </li>
 
 
         <li class="bookMark">
-            <a href="/bookmark"><i class="fa-solid fa-map-pin fa-2x"></i><br>
+            <a href="/bookmark"><i class="fa-regular fa-bookmark fa-2x"></i><br>
             <span class="iconName">旅のしおり</span></a>
         </li>
 
         
         <li class="model">
-            <a href="/model"><i class="fa-solid fa-road fa-2x"></i><br>
+            <a href="/model"><i class="fa-solid fa-route fa-2x"></i><br>
             <span class="iconName">モデルコース</span></a>
         </li>
 
         <li class="ranking">
-            <a href="/ranking"><i class="fa-solid fa-crown fa-2x"></i><br>
+            <a href="/ranking"><i class="fa-regular fa-chess-king fa-2x"></i><br>
             <span class="iconName">ランキング</span></a>
         </li>
 
         <li class="sns">
-            <a href="/smap"><i class="fa-regular fa-image fa-2x"></i><br>
+            <a href="/smap"><i class="fa-solid fa-location-dot fa-2x"></i><br>
             <span class="iconName">SNSマッピング</span></a>
         </li>
 
