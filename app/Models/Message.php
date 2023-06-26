4c2a34dec36ef9a1c41bd2699b69fa65cd6f2028
niwashizuo <84822720+niwashizuo@users.noreply.github.com>
2023-06-26 14:46:06 +0900

    メッセージtableを使用したプログラムのテストを更新

---
+++ app/Models/Message.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Message extends Model
+{
+    use HasFactory;
+    // 使用するテーブル名
+    protected $table = 'messages';
+    // 入力させないカラム
+    protected $guarded = ['id', 'created_at', 'updated_at', 'deleted_at'];
+    use SoftDeletes;
+}
