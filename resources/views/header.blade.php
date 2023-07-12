41f4193212768ad093858458420b45afc488e788
DESKTOP-R1MDEG5\shizuo <zusi0@icloud.com>
2023-07-12 20:48:09 +0900

    左上のアイコンの画像と画像サイズを変えるCSSを追加

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,7 +1,7 @@
 <div class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="map">
-      <img class="" src="img/jouetsuGo_icon.png" alt="上越GOアイコン">
+      <img id="app-icon" src="img/jouetsuGo_icon.png" alt="上越GOアイコン">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
